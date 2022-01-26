337a9c40f328a97b9a91280926eb7ea700ca37cb
LanguidBasil <e.makunev@gmail.com>
2022-01-26 22:54:40 +1000

    Reworked input processing in main

--- EnigmaMachine/Source/main.cpp
+++ EnigmaMachine/Source/main.cpp
@@ -2,17 +2,27 @@
 #include <array>
 #include "Enigma/Enigma.h"
 #include "Enigma/Rotor.h"
+#include "Math Extenstions/MathExtensions.h"
 
 int main()
 {
-	auto defaultRotorConf = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	std::array<Rotor, 3> rotors = { Rotor(defaultRotorConf, 0), Rotor(defaultRotorConf, 0), Rotor(defaultRotorConf, 0) };
+	auto IC = "DMTWSILRUYQNKFEJCAZBPGXOHV";
+	auto IIC = "HQZGPJTMOBLNCIFDYAWVEUSRKX";
+	auto IIIC = "UQNTLSZFMREHDPXKIBVYGJCWOA";
+	std::array<Rotor, 3> rotors = { Rotor(IC, 16), Rotor(IIC, 3), Rotor(IIIC, 5) };
 	Enigma e(rotors);
 
-	char input;
+	std::string input;
+	std::string output;
 	while (true)
 	{
 		std::cin >> input;
-		std::cout << ' ' << e.Encode(input) << std::endl;
+		output = "";
+		
+		for (auto c : input)
+			if (InRangeInclusive(c, 'A', 'Z'))
+				output += e.Encode(c);
+
+		std::cout << output << std::endl;
 	}
 }
